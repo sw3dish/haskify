414241e81689d55f19b445758c1bb52cb1a1b8fb
Colin Burr <colin.a.burr@gmail.com>
2017-11-04 19:15:14 -0400

    Move token and id to secrets file

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,10 @@
 
 import Haskify
 
+import Secrets
+
 main :: IO ()
 main = do
   testRequestToken
-
---User specific information:
---This needs to be replaced with values obtained from developer.spotify.com
---in order to run test that make calls to the api
-testClientId = error "must define test_client_id"
-testClientSecret = error "must define test_client_secret"
 
 testRequestToken = requestToken testClientId testClientSecret >>= print
