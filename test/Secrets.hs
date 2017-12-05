8b8df3fe4458d9e9c18f64723a6ff7128a303fe7
Colin Burr <colin.a.burr@gmail.com>
2017-12-04 23:08:53 -0500

    Add secrets as environment variables

--- test/Secrets.hs
+++ test/Secrets.hs
@@ -1,10 +1,13 @@
-module Secrets where 
+module Secrets where
 
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import qualified System.Environment as E
+import qualified System.IO.Unsafe as U
 
 --User specific information:
 --This needs to be replaced with values obtained from developer.spotify.com
 --in order to run test that make calls to the api
 testClientId, testClientSecret :: B.ByteString
-testClientId = undefined
-testClientSecret = undefined
+testClientId = C.pack (U.unsafePerformIO (E.getEnv "clientID"))
+testClientSecret = C.pack (U.unsafePerformIO (E.getEnv "clientSecret"))
